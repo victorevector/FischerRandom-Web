2bf06b507e6de6dee0de4c977695b58d783dff81
Victor E <victorevector@gmail.com>
2015-03-09 16:33:21 -0700

    added virtual env

---
+++ venv_fischer/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/victorestebanfimbres/django_workspace/fischer_random/venv_fischer/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
